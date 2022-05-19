00f0f811afc494a1f3cc23c7e555372e1ce3564b
Eugene Zboichyk <eugene.zboichyk@klarrio.com>
2022-05-19 12:24:32 -0400

    missed one

--- resources/views/partials/incidents.blade.php
+++ resources/views/partials/incidents.blade.php
@@ -24,7 +24,7 @@
                             <strong>{{ $incident->name }}</strong>{{ $incident->isScheduled ? trans("cachet.incidents.scheduled_at", ["timestamp" => $incident->scheduled_at_diff]) : null }}
                             <br>
                             <small class="date">
-                                <a href="{{ cachet_route('incident', ['id' => $incident->id]) }}" class="links"><abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->timestamp_formatted }}" data-timeago="{{ $incident->timestamp_iso }}"></abbr></a>
+                                <a href="{{ cachet_route('incident', ['incident' => $incident->id]) }}" class="links"><abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->timestamp_formatted }}" data-timeago="{{ $incident->timestamp_iso }}"></abbr></a>
                             </small>
                         </div>
                         <div class="panel-body markdown-body">
